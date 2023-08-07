810df64b8a9a0cfc6c72f017ee220a8e87b5afe4
GeorgeH <georgeharland12@gmail.com>
2023-08-07 16:59:20 +0100

    hoverover setup

--- src/script.js
+++ src/script.js
@@ -1,30 +1,19 @@
-// An empty 8x5 array for the inventory
 let inventory = new Array(5).fill(null).map(() => new Array(8).fill(null));
-
-// items?
 let items = ['Apple', 'Book', 'Flask', 'Herbs', 'Sword']
-
-// Get the grid container
 let gridContainer = document.getElementById('grid-container');
 
-// Create the grid cells
 for(let i = 0; i < inventory.length; i++) {
   for(let j = 0; j < inventory[i].length; j++) {
     let cell = document.createElement('div');
     cell.classList.add('grid-cell');
-    cell.id = `cell-${i}-${j}`; // unique id for each cell
-
-    // Assign a random item to this cell in the inventory array
+    cell.id = `cell-${i}-${j}`;
     inventory[i][j] = getRandomItem();
-
-    // If you want, you can also add the item visually to the cell here
-    // I'll assume you have a function called getItemIcon which returns an img element for an item
     let itemIcon = getItemIcon(inventory[i][j]);
     cell.appendChild(itemIcon);
-
     gridContainer.appendChild(cell);
   }
 }
+
 
 function getRandomItem() {
   let randomIndex = Math.floor(Math.random() * items.length);
@@ -32,10 +21,32 @@ function getRandomItem() {
 }
 
 function getItemIcon(item) {
+  let imgContainer = document.createElement('div');
+  
+  let highlight = document.createElement('img');
+  highlight.src = './assets/Images/InventoryMain/IMG_SlotHighlight.png';
+  highlight.style.position = 'absolute';
+  highlight.style.width = '10%';
+  highlight.style.height = '10%';
+  highlight.style.zIndex = '3';
+  highlight.style.display = 'none';
+  imgContainer.appendChild(highlight);
+
   let img = document.createElement('img');
-  img.src = `./assets/Images/Icons/${item}.png`; // or wherever your item images are stored
-  img.style.width = '50%';  // fill the grid cell
-  img.style.height = '50%'; // fill the grid cell
-  // img.style.padding = '-80%;'
-  return img;
+  img.src = `./assets/Images/Icons/${item}.png`;
+  img.style.width = '50%';
+  img.style.height = '50%';
+  img.style.zIndex = '4';
+  imgContainer.appendChild(img);
+  imgContainer.style.height = '100%';
+  imgContainer.style.width = '100%';
+
+  imgContainer.addEventListener('mouseover', function() {
+    highlight.style.display = 'block';
+  });
+  imgContainer.addEventListener('mouseout', function() {
+    highlight.style.display = 'none';
+  });
+
+  return imgContainer;
 }
